3e3a5276c721d3195675eab2adff2f22c8404709
AsFLY-902 <37661597+AsFLY-902@users.noreply.github.com>
2020-05-12 14:16:46 +0530

    11-05-2020 [Q4 - Count of paths in a grid]

--- Ashok/11 May/CountPaths.cpp
+++ Ashok/11 May/CountPaths.cpp
@@ -3,26 +3,27 @@
   without crossing the major diagonal.
 */
 
-#include <iostream> 
-#include <cstring> 
+#include <iostream>
+#include <cstring>
 
-using namespace std; 
+using namespace std;
 
 #define ALLOC(size,dp) memset(dp,0,size*sizeof(*dp));
 
 int main(){
 	int size;
 	cout << "Enter number of rows and columns:" << endl;
-  cin >> size;
-  int *dp = new int[size];
-  ALLOC(size,dp)
-  dp[0] = 1; 
-  for (int i = 1; i < size; i++){ 
-        for (int j = 1; j < size; j++){ 
-          if(j<=i) dp[j] += dp[j - 1];
-		  else break; 
-        } 
-    } 
-    cout << "Output: "<<dp[size-1]; 
+	cin >> size;
+	int *dp = new int[size];
+	ALLOC(size,dp)
+	dp[0] = 1;
+	for (int i = 1; i < size; i++){
+            for (int j = 1; j < size; j++){
+                if(j<=i) dp[j] += dp[j - 1];
+    else break;
+        }
+    }
+    cout << "Output: "<<dp[size-1];
     delete dp;
-} 
+}
+
